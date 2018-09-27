b2095724c01074060cbf5e1123822b7fc7e724e9
mhanley00 <mehanley2@gmail.com>
2018-09-27 19:35:17 -0400

    put everything into index for easy testing

--- index.js
+++ index.js
@@ -4,7 +4,8 @@
 var express = require("express");
 var Words = require("./Word.js");
 var inquirer = require("inquirer");
-
+//chalk for color
+//look into push and pop, and making it a string
 // ______________________________________________________________________________
 // EXPRESS - server setup
 // -----–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
@@ -58,10 +59,12 @@ app.listen(PORT, function() {
 // newClass.addStudent("Keiyon", "Full Stack", 4.0);
 
 // console.log(newClass);
+var words = ["dog", "cat"];
+var currentWordG = words[Math.floor(Math.random())*words.length];
 
-var userGuess = process.argv[2];
+var userGuess = 'p'; //process.argv[2];
 
-function Letter(letter) {
+function Letter(letter) {// word[i] ie a letter from the word
     this.letter = letter; 
     this.show = false; //default do not show the actual letter
     this.showLetter = function(){
@@ -73,14 +76,43 @@ function Letter(letter) {
             return this.letter;
     }
   };
-  this.checkLetUDstring = function (){
+  this.checkLetUDstring = function (userGuess){
       if (userGuess === this.letter){
           this.show = true;
+          Letter(userGuess);
       }
   }
 };
-console.log(userGuess);
-var myGuess = new Letter ('p');
-console.log(userGuess.checkLetUDstring());
-var anotherGuess = new Letter('r');
-console.log(myGuess.showLetter);
+
+
+console.log("userGuess: " + userGuess);
+// var theLetter = new Letter ('p');
+
+// var userGuess = new Letter(process.argv[2]);
+
+// want to see if theLetter == userGuess aka 'p'
+// theLetter.checkLetUDstring(userGuess);
+// console.log(theLetter.show);
+
+// module.exports = Letter;
+// function Word(word){
+//     this.wordArr = [];
+//     for (var i = 0; i< word.length; i++){
+//         console.log(word[i]);
+//         var eachLetter = new Letter(word[i]); //each letter in the word
+//         eachLetter.showLetter(); //replacing each letter with #### until each letter gets guessed
+//         this.wordArr.push(eachLetter); //pushing our new word into #### until each letter gets guessed
+//     }
+//     this.guess = function (userGuess){
+//         for (var i=0; i<this.wordArr.length; i++){
+//             this.wordArr[i].checkLetUDstring(userGuess); //ze function from Letter constructor
+//         }
+//     }
+    
+// }
+
+// Word(currentWordG);
+console.log("currentWordG: " + currentWordG);
+//   var daWord = new Word ('dog');
+//   console.log(daWord.wordArr.toString()); // return whole thing as a string instead of commas between
+
