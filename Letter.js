24b1c442724dc0bd6aa3017e181f1bfb4d2a6ea7
mhanley00 <mehanley2@gmail.com>
2018-09-27 16:01:43 -0400

    added more word stuff

--- Letter.js
+++ Letter.js
@@ -4,9 +4,9 @@
 
 //   * A boolean value that stores whether that letter has been guessed yet✅
 
-//   * A function that returns the underlying character if the letter has been guessed, or a placeholder (like an underscore) if the letter has not been guessed
+//   * A function that returns the underlying character if the letter has been guessed, or a placeholder (like an underscore) if the letter has not been guessed✅
 
-//   * A function that takes a character as an argument and checks it against the underlying character, updating the stored boolean value to true if it was guessed correctly
+//   * A function that takes a character as an argument and checks it against the underlying character, updating the stored boolean value to true if it was guessed correctly✅
 
 
 function Letter(letter) {
--- Word.js
+++ Word.js
@@ -1,20 +1,33 @@
+// * **Word.js**: Contains a constructor, Word that depends on the Letter constructor. This is used to create an object representing the current word the user is attempting to guess. That means the constructor should define:
+
+//   * An array of `new` Letter objects representing the letters of the underlying word✅
+
+//   * A function that returns a string representing the word. This should call 
+//the function on each letter object (the first function defined in `Letter.js`) 
+//that displays the character or an underscore and concatenate those together.
+
+//   * A function that takes a character as an argument and calls the guess function
+// on each letter object (the second function defined in `Letter.js`)✅
+
 var Letter = require("./Letter.js");
 
 function Word(word){
     this.wordArr = [];
     for (var i = 0; i< word.length; i++){
-      console.log(word [i]);
-      var anotherWord = new Letter(word[i]);
-      this.wordArr.push(anotherWord);
-    } //need 1 more for loop!
+      console.log(word[i]);
+      var eachLetter = new Letter(word[i]); //each letter in the word
+      eachLetter.showLetter(); //replacing each letter with #### until each letter gets guessed
+      this.wordArr.push(eachLetter); //pushing our new word into #### until each letter gets guessed
+  }
     this.guess = function (userGuess){
       for (var i=0; i<this.wordArr.length; i++){
-        this.wordArr[i].checkLetUDstring(userGuess);
+        this.wordArr[i].checkLetUDstring(userGuess); //ze function from Letter constructor
       }
     }
-  }
+
+}
   
   var daWord = new Word ('dog');
-  console.log(daWord.wordArr) // what would this do?
+  console.log(daWord.wordArr.toString()); // return whole thing as a string instead of commas between
 
 module.exports = Letter;
